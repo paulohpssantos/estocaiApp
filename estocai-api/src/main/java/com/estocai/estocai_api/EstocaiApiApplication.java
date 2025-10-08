59b4354c7a77746f8b2ee6bd8e5ad2631806c5c8
Paulo Henrique Pereira dos Santos <84387138+paulohpssantos@users.noreply.github.com>
2025-10-08 14:53:01 -0300

    Adicionado api

---
+++ estocai-api/src/main/java/com/estocai/estocai_api/EstocaiApiApplication.java
@@ -0,0 +1,13 @@
+package com.estocai.estocai_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstocaiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstocaiApiApplication.class, args);
+	}
+
+}
---
+++ estocai-api/src/test/java/com/estocai/estocai_api/EstocaiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estocai.estocai_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstocaiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
