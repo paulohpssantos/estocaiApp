1dc8f28d25ca376288c38d685865931aae9de908
Paulo Henrique Pereira dos Santos <84387138+paulohpssantos@users.noreply.github.com>
2025-11-21 19:00:58 -0300

    Update ProdutoOrdemServicoRepository.java

--- estocai-api/src/main/java/com/estocai/estocai_api/repository/ProdutoOrdemServicoRepository.java
+++ estocai-api/src/main/java/com/estocai/estocai_api/repository/ProdutoOrdemServicoRepository.java
@@ -3,6 +3,8 @@ package com.estocai.estocai_api.repository;
 import com.estocai.estocai_api.model.ProdutoOrdemServico;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -11,5 +13,8 @@ public interface ProdutoOrdemServicoRepository extends JpaRepository<ProdutoOrde
 
     List<ProdutoOrdemServico> findByOrdemServicoId(Long ordemServicoId);
 
-    int deleteByOrdemServicoId(Long ordemServicoId);
+    @Modifying
+    @Transactional
+    @Query("delete from ProdutoOrdemServico p where p.ordemServico.id = :ordemId")
+    int deleteByOrdemServicoId(@Param("ordemId") Long ordemId);
 }
